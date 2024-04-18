86c1c624318f5c834e63998f633b414e2682d594
S573647 <S573647@nwmissouri.edu>
2024-04-17 23:28:07 -0500

    app.py is commented

--- app/app.py
+++ app/app.py
@@ -4,22 +4,29 @@ from faicons import icon_svg
 from shiny import reactive
 from shiny.express import input, render, ui
 import palmerpenguins 
-
+# load penguines data from palmerpenguins package into a data frame
 df = palmerpenguins.load_penguins()
 
+# create  penguins dashboard
 ui.page_opts(title="Penguins dashboard", fillable=True)
 
-
+# create a sidebar with title Filter controls
 with ui.sidebar(title="Filter controls"):
+    # create a slider for mass between 2000 and 6000 with default 6000 selected
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+    # create multiple check boxes and select them
     ui.input_checkbox_group(
         "species",
         "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
+
+    # draw a horizontal line
     ui.hr()
+    # create subheader named Links
     ui.h6("Links")
+    # create hyperlink for different things
     ui.a(
         "GitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
@@ -47,22 +54,23 @@ with ui.sidebar(title="Filter controls"):
         target="_blank",
     )
 
-
+# create a row with multiple layout columns
 with ui.layout_column_wrap(fill=False):
+    # first column
     with ui.value_box(showcase=icon_svg("earlybirds")):
         "Number of penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
-
+    # second column
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
         "Average bill length"
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
-
+    # third column
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
         "Average bill depth"
 
@@ -70,8 +78,9 @@ with ui.layout_column_wrap(fill=False):
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
-
+# create a row with multiple columns
 with ui.layout_columns():
+    # first column
     with ui.card(full_screen=True):
         ui.card_header("Bill length and depth")
 
@@ -83,7 +92,7 @@ with ui.layout_columns():
                 y="bill_depth_mm",
                 hue="species",
             )
-    # card for Penguine data
+    # second column
     with ui.card(full_screen=True):
         ui.card_header("Penguin data")
 
@@ -101,7 +110,7 @@ with ui.layout_columns():
 
 #ui.include_css(app_dir / "styles.css")
 
-
+#Filter data at run time and display live results based on it.
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
